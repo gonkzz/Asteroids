12c120fe2772f26c208cb7615cf34cf4400ae109
gonks <gs.goncalosoares@gmail.com>
2025-05-18 14:04:27 +0200

    Player moving left and right

--- main.py
+++ main.py
@@ -2,13 +2,23 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
+    # initialize all pygame imported modules
     pygame.init()
 
+    # create new GUI window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # object to help track time
+    clock = pygame.time.Clock()
+    # delta time, time passed since last frame was drawn
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # game loop
     while True:
@@ -18,9 +28,18 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        # fill black screen
         screen.fill("black")
+        # re-render player each frame
+        player.draw(screen)
+        player.update(dt)
+        # refresh the screen
         pygame.display.flip()
+        # pause the loop until 1/60th of a sec have passed
+        # store that time in milliseconds inside dt
+        dt = clock.tick(60) / 1000
 
+    # Print text out to the console
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
---
+++ player.py
@@ -0,0 +1,35 @@
+import pygame
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from circleshape import CircleShape
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    # player will look like a triangle,
+    # even though its hitbox is represented as a circle
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(
+            self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    # draw triangle(player) on the screen
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
